f2b7e19437eb94565a42a7b3bc4319ead1b73342
Salyakin <Salyakin666@gmail.com>
2024-12-01 15:37:09 +0500

    01.12.2024

--- Modul_1_3.py
+++ Modul_1_3.py
@@ -1,9 +1,17 @@
-name = "Иван"
-print ('Меня зовут: ', name)
-age = 25
-print ('Мне: ',age, 'лет')
-age = age + 2
-print ('New_age: ', age)
-is_student = True
-print ('Is_student:', is_student)
+##name = "Иван"
+##print ('Меня зовут: ', name)
+##age = 25
+##print ('Мне: ',age, 'лет')
+##age = age + 2
+##print ('New_age: ', age)
+##is_student = True
+##print ('Is_student:', is_student)
+
+##Переменные
+count_DZ = 12
+count_chas = 1.5
+titl_kurs = 'Python'
+time_1DZ = count_chas / count_DZ
+print ('Курс:',titl_kurs+',','всего задач:',str(count_DZ)+',','затрачено часов:', str(count_chas)+',','среднее время выполнения:',time_1DZ, 'часа')
+
 
